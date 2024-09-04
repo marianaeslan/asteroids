d016d720cbb7d3b25d89c84c35e0cb02fb335398
marianaeslan <mariana.eslan@gmail.com>
2024-09-04 10:39:50 -0400

    printing triangle on the screen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -9,16 +10,24 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.display.set_mode()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2, radius=PLAYER_RADIUS)
+    dt = 0
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
+        dt = game_clock.tick(60) / 1000
         pygame.display.update()
+        
     pygame.quit()
+
 if __name__ == "__main__":
     main()
 
