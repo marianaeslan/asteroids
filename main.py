8182706ed5ddde785c43e3fe8afe7a839a912739
marianaeslan <mariana.eslan@gmail.com>
2024-09-05 20:54:36 -0400

    check collision between bullet and asteroid

--- main.py
+++ main.py
@@ -53,6 +53,12 @@ def main():
                 print("Game over!")
                 exit() 
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.check_collision(asteroid):
+                    shot.kill()
+                    asteroid.kill()
+                     
         # Draw all objects in the 'drawable' group
         for obj in drawable:
             obj.draw(screen)
